673d9a5417095a0ad9427b1324740925adcf33e1
anthony-akbar <mamadjanovakbar@gmail.com>
2023-02-17 16:58:33 +0500

    Slider double fix

--- resources/views/front/partials/hero.blade.php
+++ resources/views/front/partials/hero.blade.php
@@ -1,11 +1,13 @@
-<!-- ======= Hero Section ======= -->
 <section id="hero">
     <div class="hero-container">
         <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
+
             <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
+
             <div class="carousel-inner" role="listbox">
-                @foreach($sliderItems as $sliderItem)
-                    <div class="carousel-item{{ $toggle ? ' active' : '' }}" style="background-image: url({{ 'storage/' . $sliderItem->image }});">
+                <!-- Slides -->
+                @foreach($sliderItems as $key => $sliderItem)
+                    <div class="carousel-item {{ $key == 0 ? 'active' : '' }}" style="background-image: url({{ 'storage/' . $sliderItem->image }});">
                         <div class="carousel-container">
                             <div class="carousel-content">
                                 <h2 class="animate__animated animate__fadeInDown">{{ $sliderItem->title }}</h2>
@@ -17,11 +19,17 @@
                             </div>
                         </div>
                     </div>
-                        <?php $toggle = false; ?>
                 @endforeach
             </div>
-            <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev"><span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span></a>
-            <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next"><span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span></a>
+
+            <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+            </a>
+
+            <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+            </a>
+
         </div>
     </div>
 </section><!-- End Hero -->
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -3,18 +3,14 @@
 <head>
     <meta charset="utf-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-
     <title>Bros_Coffee</title>
     <meta content="" name="description">
     <meta content="" name="keywords">
-
     <!-- Favicons -->
     <link href="{{ asset('assets/img/svgexport-8.svg')}}" rel="icon">
     <link href="{{ asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
-
     <!-- Google Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
-
     <!-- Vendor CSS Files -->
     <link rel="stylesheet" href="{{ asset('assets/vendor/animate.css/animate.min.css')}}">
     <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}">
@@ -27,7 +23,6 @@
     <link rel="stylesheet" href="{{ asset('assets/css/style.css')}}" >
 
     <link rel="icon" type="image/icon" href="{{ asset('svgexport-.svg')}}">
-    <title>Royal</title>
     @yield('styles')
 </head>
 <body>
@@ -36,7 +31,6 @@
 
 <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-<script src="{{asset('assets/js/main.js')}}"></script>
 <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
 <script src="{{ asset('assets/vendor/glightbox/js/glightbox.min.js') }}"></script>
 <script src="{{ asset('assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
